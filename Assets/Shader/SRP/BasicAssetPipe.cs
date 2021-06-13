754ccba6190f4e79158a0e4922b11df7396fe8b4
NyPrivate <trident0310@gmail.com>
2021-06-14 08:25:17 +0900

    SRP

--- Assets/Shader/SRP/BasicAssetPipe.cs
+++ Assets/Shader/SRP/BasicAssetPipe.cs
@@ -2,44 +2,49 @@
 using UnityEngine.Rendering;
 using UnityEngine.Experimental.Rendering;
 
+// スクリプトを Edit モードで実行
+[ExecuteInEditMode]
 public class BasicAssetPipe : RenderPipelineAsset
 {
-    public Color ClearColor = Color.green;
+    public Color clearColor = Color.green;
+    protected override RenderPipeline CreatePipeline()
+    {
+       return new BasicPipeInstance(clearColor);
+    }
 
-#if UNITY_EDITOR
+    #if UNITY_EDITOR
     [UnityEditor.MenuItem("SRP-Demo/01 - Create Basic Asset Pipeline")]
     static void CraeteBasicAssetPipeline()
     {
+        // このクラスのインスタンスを作成
         var instance = ScriptableObject.CreateInstance<BasicAssetPipe>();
-        UnityEditor.AssetDatabase.CreateAsset(instance, "Assets/SRP-Demo/1-BasicAssetPipe.asset");
+        // アセット化
+        UnityEditor.AssetDatabase.CreateAsset(instance, "Assets/Shader/SRP/1-BasicAssetPipe.asset");
     }
 #endif
-
-    protected override IRenderPipeline InternalCreatePipeline()
-    {
-        return new BasicPipeInstance(ClearColor);
-    }
 }
 
 public class BasicPipeInstance : RenderPipeline
 {
-    private Color _clearColor = Color.black;
+    private Color m_ClearColor = Color.black;
 
     public BasicPipeInstance(Color clearColor)
     {
-        _clearColor = clearColor;
+        m_ClearColor = clearColor;
     }
 
-    public override void Render(ScriptableRenderContext context, Camera[] cameras)
+    /// <summary>
+    /// 描画処理
+    /// </summary>
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
-        // does not so much yet.
-        base.Render(context, cameras);
-
-        // Clear buffer to the configured color.
         var cmd = new CommandBuffer();
-        cmd.ClearRenderTarget(true, true, _clearColor);
+        // レンダーターゲットを設定した色で塗りつぶす
+        cmd.ClearRenderTarget(true, true, m_ClearColor);
+        // コマンドバッファを即時実行
         context.ExecuteCommandBuffer(cmd);
         cmd.Release();
+        // スケジュールされたすべてのコマンドをレンダリングループに送信して実行します。
         context.Submit();
     }
 }

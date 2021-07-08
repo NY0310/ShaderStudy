364a1a1d8f933d4f33bb626f05a3290bbb28e99e
NyPrivate <trident0310@gmail.com>
2021-07-08 19:04:24 +0900

    ランバート

---
+++ Assets/Shader/Util/CameraFilter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraFilter : MonoBehaviour
+{
+    [SerializeField] private Material filter;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src,dest,filter);
+    }
+}
